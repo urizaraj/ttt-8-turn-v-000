8650ab65f8c086bf9a493f239c722500ca2ea897
Alexander Urizar <urizaraj@gmail.com>
2018-02-08 16:59:16 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,45 +1,40 @@
 # returns whether a valid move
 def valid_move?(board, index)
-  if (0..8) === index
-    if !position_taken?(board, index)
-      return true
-    end
-  end
-  return false
+  return true if (0..8) === index && !position_taken?(board, index)
+  false
 end
 
-#returns whether position on board is taken
+# returns whether position on board is taken
 def position_taken?(board, index)
   space = board[index]
-  if space == 'X' || space == "O"
+  if space == 'X' || space == 'O'
     return true
   else
     return false
   end
 end
 
-def move(board, index, token = "X")
+def move(board, index, token = 'X')
   board[index] = token
 end
 
 # displays the board
 def display_board(board)
-  [0,3,6].each do |i|
+  [0, 3, 6].each do |i|
     puts " #{board[i]} | #{board[i + 1]} | #{board[i + 2]} "
-    if i != 6
-      puts '-----------'
-    end
+    puts '-----------' if i != 6
   end
 end
 
 def turn(board)
-  while true
-    puts "Please enter 1-9:"
-    input = gets().strip()
+  loop do
+    puts 'Please enter 1-9:'
+    input = gets.strip
     index = input_to_index(input)
+    move(board, index) if valid_move?(board, index)
   end
 end
 
 def input_to_index(input)
-  return input.to_i() - 1
+  input.to_i - 1
 end
