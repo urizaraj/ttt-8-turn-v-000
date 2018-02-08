8785836bc61a7a303b7a0be45290b9638d4a0fb7
Alexander Urizar <urizaraj@gmail.com>
2018-02-08 16:39:59 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,17 @@
+def valid_move?(board, index)
+  if (0..8) === index
+    if !position_taken?(board, index)
+      return true
+    end
+  end
+  return false
+end
+
+def position_taken?(board, index)
+  space = board[index]
+  if space == 'X' || space == "O"
+    return true
+  else
+    return false
+  end
+end
